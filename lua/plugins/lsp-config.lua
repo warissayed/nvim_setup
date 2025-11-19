ca200d2ce1566663813d902e6699950a80a964b4
warissayed <warissayed72@gmail.com>
2025-11-20 03:07:08 +0530

    Update telescope and lsp files

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,6 +1,6 @@
 return {
   {
-"williamboman/mason.nvim",
+    "williamboman/mason.nvim",
     config = function()
       require("mason").setup()
     end
@@ -8,9 +8,21 @@ return {
   {
     "williamboman/mason-lspconfig.nvim",
     config = function()
-    require("mason-lspconfig").setup({
-      ensure_installed = {"lua_ls"}
+      require("mason-lspconfig").setup({
+        ensure_installed = { "lua_ls" }
       })
+    end
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+      vim.keymap.set('n','K', vim.lsp.buf.hover,{})
+      vim.keymap.set('n','gd',vim.lsp.buf.definition,{})
+      vim.keymap.set({'n','v'} , '<leader>ca' , vim.lsp.buf.code_action,{})
     end
   }
 }
+
+
